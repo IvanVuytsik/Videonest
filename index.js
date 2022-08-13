f45c903cfaa1195fcfec7fcf50daf6a5974811df
Ivan Vuytsik <vuicik1989@gmail.com>
2022-08-13 06:15:04 +0300

    make it better

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 
 import cookieParser from "cookie-parser";
+
 import path from "path";
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 dotenv.config();
