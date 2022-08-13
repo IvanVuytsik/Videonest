871078b33ed9958538ee084f7042a39bd9839462
Ivan Vuytsik <vuicik1989@gmail.com>
2022-08-13 06:52:17 +0300

    make it better

--- client/src/Firebase.js
+++ client/src/Firebase.js
@@ -4,7 +4,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // console.log(FIREBASE_API)
 
 const firebaseConfig = {
-  apiKey: String(process.env.FIREBASE_API),
+  apiKey: "AIzaSyC5QnZ-hScJH5FJlEUCQEri7xmGHEqX02k",
   authDomain: "videonest-1d435.firebaseapp.com",
   projectId: "videonest-1d435",
   storageBucket: "videonest-1d435.appspot.com",
