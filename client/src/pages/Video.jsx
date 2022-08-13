689b39742fc911c3ed6e4699bfdf6a267f6a8fb8
Ivan Vuytsik <vuicik1989@gmail.com>
2022-08-13 18:06:01 +0300

    make it better

--- client/src/pages/Video.jsx
+++ client/src/pages/Video.jsx
@@ -208,13 +208,13 @@ function Video() {
         {currentUser && 
           <Buttons>
               <Button onClick={handleLike}>
-                {video.likes.includes(channel._id) ? (<ThumbUpIcon />) : (<ThumbUpOffAltIcon />)}
+                {video.likes.includes(String(channel._id)) ? (<ThumbUpIcon />) : (<ThumbUpOffAltIcon />)}
                 {" "}
                 {(video.likes).length}
               </Button>
 
               <Button onClick={handleDislike}>
-                {video.dislikes.includes(channel._id) ? (<ThumbDownIcon />) : (<ThumbDownOffAltIcon />)}
+                {video.dislikes.includes(String(channel._id)) ? (<ThumbDownIcon />) : (<ThumbDownOffAltIcon />)}
                 {" "}
                 Dislike
               </Button>
